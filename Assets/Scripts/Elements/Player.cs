00561ee7bcd074adada37943e83cce1fbee39ff7
gokcegun <gokce@locomotive.games>
2025-01-23 22:18:08 +0300

    Ders 2

--- Assets/Scripts/Elements/Player.cs
+++ Assets/Scripts/Elements/Player.cs
@@ -1,16 +1,43 @@
+using System.Xml;
+using UnityEditor;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float speed;
+    private void Update()
     {
-        
+        var direction = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction += Vector3.forward;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction += Vector3.right;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction += Vector3.back;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction += Vector3.left;
+        }
+        transform.position += direction.normalized * speed * Time.deltaTime;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        transform.position = transform.position + Vector3.forward * .01f;
+    private void OnTriggerEnter(Collider other)
+    {       
+        if (other.CompareTag("Enemy"))
+        {
+            gameObject.SetActive(false);
+        }
+        if (other.CompareTag("Apple"))
+        {
+            var apple = other.gameObject;
+            apple.SetActive(false);
+        }
     }
 }
